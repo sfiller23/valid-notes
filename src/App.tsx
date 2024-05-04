1e9fd430301c283328b2b5b093e25a17db2e0686
sfiller23 <sf27lp@gmail.com>
2024-05-04 21:05:04 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  CyclicLinkedList,
+  Node as Gnode,
+  Intervals,
+  MusicalScale,
+  notes,
+} from "./utils/note";
+
+function App() {
+  const [chord, setChord] = useState<string>("");
+  const [currentValidNotes, setCurrentValidNotes] = useState<Set<string>>(
+    new Set<string>()
+  );
+
+  const numRows = 6;
+  const numColumns = 12;
+
+  const tuning: string[] = ["E", "A", "D", "G", "B", "E"];
+
+  const fretBoardNotes = new CyclicLinkedList();
+
+  fretBoardNotes.buildList(notes);
+
+  const currentScaleSchema: Intervals = [2, 2, 1, 2, 2, 2, 1];
+
+  const generateAllScales = () => {
+    const allScales: string[][] = [];
+
+    notes.forEach((note) => {
+      const scale = new MusicalScale(note, currentScaleSchema);
+      allScales.push(scale.getNotes());
+    });
+
+    return allScales;
+  };
+
+  const allScalesMatrix = generateAllScales();
+
+  const validNotes = new Set<string>();
+
+  const getNotes = () => {
+    const currentChord = chord.toUpperCase().split(",");
+
+    const validScales = allScalesMatrix.filter((scale) => {
+      return currentChord.every((note) => scale.includes(note));
+    });
+
+    validScales.forEach((scale) => {
+      scale.forEach((note) => {
+        validNotes.add(note);
+      });
+    });
+
+    setCurrentValidNotes(validNotes);
+  };
+
+  const renderGrid = () => {
+    const grid = [];
+
+    for (let row = 1; row < numRows + 1; row++) {
+      const rowItems = [];
+      const currentNode: Gnode | null = fretBoardNotes.getNextNode(
+        tuning[row - 1]
+      );
+
+      const currentRow = new CyclicLinkedList(currentNode);
+      for (let col = numColumns; col > 0; col--) {
+        rowItems.push(
+          <span key={`${row}-${col}`} className="grid-item">
+            <button
+              style={{
+                backgroundColor: `${
+                  currentValidNotes.has(
+                    currentRow?.getNodeByIndex(col)?.value as string
+                  )
+                    ? "green"
+                    : "grey"
+                }`,
+                margin: "5px",
+              }}
+              type="button"
+            >
+              <div>{col}</div>
+              <div>{currentRow.getNodeByIndex(col)?.value}</div>
+            </button>
+          </span>
+        );
+      }
+
+      grid.push(
+        <div key={row} className="grid-row">
+          {rowItems}
+        </div>
+      );
+    }
+
+    return grid;
+  };
+
+  return (
+    <>
+      <label htmlFor="chord">Please Input Chord Notes : </label>
+      <input
+        id="chord"
+        type="text"
+        value={chord}
+        onChange={(e) => {
+          setChord(e.target.value);
+        }}
+      />
+      <button
+        style={{ margin: "5px", backgroundColor: "blue" }}
+        onClick={getNotes}
+        type="button"
+      >
+        Get Notes
+      </button>
+      <div className="grid-container">{renderGrid()}</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/note.ts
@@ -0,0 +1,271 @@
+export const notes = [
+  "A",
+  "A#",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "D#",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "G#",
+];
+
+export class Node {
+  value: string;
+  next: Node | null;
+
+  constructor(value: string) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+export class CyclicLinkedList {
+  head: Node | null;
+  tail: Node | null;
+
+  constructor(node?: Node | null) {
+    this.head = node ?? null;
+    this.tail = null;
+  }
+
+  isEmpty(): boolean {
+    return this.head === null;
+  }
+
+  insert(value: string): void {
+    const newNode = new Node(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = newNode;
+    } else {
+      newNode.next = this.head;
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+  }
+  getNodeByIndex(index: number): Node | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while (currentNode !== null && currentIndex !== index) {
+      currentNode = currentNode.next;
+      currentIndex++;
+      if (currentNode === this.head) {
+        break;
+      }
+    }
+    return currentNode;
+  }
+
+  getNextNode(value: string): Node | null {
+    if (this.isEmpty()) {
+      return null;
+    }
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+      if (currentNode === this.head) {
+        break;
+      }
+    }
+    return null;
+  }
+
+  buildList(values: string[]): void {
+    values.forEach((value) => this.insert(value));
+  }
+
+  remove(value: string): void {
+    if (this.isEmpty()) {
+      return;
+    }
+
+    let currentNode = this.head;
+    let prevNode = null;
+
+    while (currentNode !== null) {
+      if (currentNode.value === value) {
+        if (currentNode === this.head) {
+          this.head = currentNode.next;
+          this.tail!.next = this.head;
+        } else if (currentNode === this.tail) {
+          prevNode!.next = this.head;
+          this.tail = prevNode;
+        } else {
+          prevNode!.next = currentNode.next;
+        }
+        return;
+      }
+
+      prevNode = currentNode;
+      currentNode = currentNode.next;
+
+      if (currentNode === this.head) {
+        break;
+      }
+    }
+  }
+
+  print(): void {
+    if (this.isEmpty()) {
+      console.log("The cyclic linked list is empty.");
+      return;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      console.log(currentNode.value);
+      currentNode = currentNode.next;
+
+      if (currentNode === this.head) {
+        break;
+      }
+    }
+  }
+}
+
+export class Note {
+  private frequency: number;
+  private sustain: {
+    volume: number;
+    duration: number;
+  };
+  private symbol: string;
+
+  constructor(
+    symbol: string,
+    frequency?: number,
+    volume?: number,
+    duration?: number
+  ) {
+    this.frequency = frequency ?? 0;
+    this.sustain = {
+      volume: volume ?? 0,
+      duration: duration ?? 0,
+    };
+    this.symbol = symbol;
+  }
+
+  getSymbol(): string {
+    return this.symbol;
+  }
+
+  setSymbol(symbol: string): void {
+    this.symbol = symbol;
+  }
+
+  getFrequency(): number {
+    return this.frequency;
+  }
+
+  setFrequency(frequency: number): void {
+    this.frequency = frequency;
+  }
+
+  getDuration(): number {
+    return this.sustain.duration;
+  }
+
+  setDuration(duration: number): void {
+    this.sustain.duration = duration;
+  }
+
+  getVolume(): number {
+    return this.sustain.volume;
+  }
+
+  setVolume(volume: number): void {
+    this.sustain.volume = volume;
+  }
+
+  setSustain(volume: number, duration: number): void {
+    this.sustain = {
+      volume: volume,
+      duration: duration,
+    };
+  }
+
+  getSustain(): { volume: number; duration: number } {
+    return this.sustain;
+  }
+}
+
+export type Intervals = [
+  number,
+  number,
+  number,
+  number,
+  number,
+  number,
+  number
+];
+
+export class MusicalScale {
+  private root: string;
+  private intervals: Intervals;
+  private notes: string[];
+
+  constructor(root: string, intervals: Intervals) {
+    let currentIndex = notes.indexOf(root);
+    const scaleNotes = [
+      root,
+      ...intervals.map((interval) => {
+        const noteIndex = (currentIndex + interval) % notes.length;
+        currentIndex = noteIndex;
+        return notes[noteIndex];
+      }),
+    ];
+    scaleNotes.pop();
+    this.notes = scaleNotes;
+    this.root = root;
+    this.intervals = intervals;
+  }
+
+  getNotes(): string[] {
+    return this.notes;
+  }
+
+  //   addNote(note: Note): void {
+  //     this.notes.push(note);
+  //   }
+
+  //   removeNoteAtIndex(index: number): void {
+  //     this.notes.splice(index, 1);
+  //   }
+
+  //   getNoteAtIndex(index: number): Note {
+  //     return this.notes[index];
+  //   }
+
+  //   getNoteCount(): number {
+  //     return this.notes.length;
+  //   }
+
+  //   getFrequencies(): number[] {
+  //     return this.notes.map((note) => note.getFrequency());
+  //   }
+
+  //   getDurations(): number[] {
+  //     return this.notes.map((note) => note.getDuration());
+  //   }
+
+  //   getVolumes(): number[] {
+  //     return this.notes.map((note) => note.getVolume());
+  //   }
+
+  //   getSustains(): { volume: number; duration: number }[] {
+  //     return this.notes.map((note) => note.getSustain());
+  //   }
+}
