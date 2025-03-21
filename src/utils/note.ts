3d9170284d51c6f1a863c19587e73c9e7671cd9f
shimon <sf27lp@gmail.com>
2025-03-21 12:59:22 +0200

    implement getters for the root and intervals

--- src/utils/note.ts
+++ src/utils/note.ts
@@ -213,6 +213,7 @@ export type Intervals = [
 ];
 
 export class MusicalScale {
+  // for future use
   private root: string;
   private intervals: Intervals;
   private notes: string[];
@@ -237,6 +238,15 @@ export class MusicalScale {
     return this.notes;
   }
 
+  getRoot(): string {
+    return this.root;
+  }
+
+  getIntervals(): Intervals {
+    return this.intervals;
+  }
+
+  // for future use:
   //   addNote(note: Note): void {
   //     this.notes.push(note);
   //   }
